cc3d53c5600ab339d4043c713d333ba3bd311c02
Ronak Ramachandran <ironak314@gmail.com>
2016-04-22 16:36:57 -0400

    Added content back
    This all used to be in another repository which I had to delete because
    it accidentally contained sensitive data.

---
+++ EditHtmlFiles.java
@@ -0,0 +1,116 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class EditHtmlFiles
+{
+	public static void main( String[] args )
+	{
+		// Reading ==================================================================================================================================
+		
+		ArrayList<String> lines = readLines( "editing/base.html" );
+		
+		ArrayList<String> titles = readLines( "editing/titles.txt" );
+		ArrayList<String> mouseOver = readLines( "editing/mouseOver.txt" );
+		
+		// Writing ==================================================================================================================================
+		
+		int maxNumber = 0013;
+		String fileName;
+		
+		for( int j = 1; j <= maxNumber; j++ )
+		{
+			fileName = "noodles/" + String.format( "%04d", j ) + ".html";
+			
+			lines.set( 10, "		<h2>" + titles.get( j - 1 ) + "</h2>" );
+			lines.set( 11, "		<img src=\"../comics/comic" + String.format( "%04d", j ) + ".JPG\" width=\"400\" height=\"400\" alt=\"" + mouseOver.get( j - 1 ) + "\">" );
+			
+			if( j == 1 )
+			{
+				lines.set( 12, "		<p><a href=\"0002.html\">NEXT</a>|<a href=\"../index.html\">LAST</a><p>" );
+			}
+			else if( j == maxNumber )
+			{
+				lines.set( 12, "		<p><a href=\"0001.html\">FIRST</a>|<a href=\"" + String.format( "%04d", j - 1 ) + ".html\">PREV</a>|<a href=\"../index.html\">LAST</a><p>" );
+			}
+			else
+			{
+				lines.set( 12, "		<p><a href=\"0001.html\">FIRST</a>|<a href=\"" + String.format( "%04d", j - 1 ) + ".html\">PREV</a>|<a href=\"" + String.format( "%04d", j + 1 ) + ".html\">NEXT</a>|<a href=\"../index.html\">LAST</a><p>" );
+			}
+			
+			writeLines( lines, fileName );
+		}
+		
+		//index.html edits
+		lines.set( 4, "		<link rel=\"stylesheet\" href=\"style.css\">" );
+		lines.set( 9, "		<h3><a href=\"index.html\"><strong>Home</strong></a>|<a href=\"about.html\">About US</a></h3>" );
+		lines.set( 10, "		<h2>" + titles.get( maxNumber - 1 ) + "</h2>" );
+		lines.set( 11, "		<img src=\"comics/comic" + String.format( "%04d", maxNumber ) + ".JPG\" width=\"400\" height=\"400\" alt=\"" + mouseOver.get( maxNumber - 1 ) + "\">" );
+		lines.set( 12, "		<p><a href=\"noodles/0001.html\">FIRST</a>|<a href=\"noodles/" + String.format( "%04d", maxNumber - 1 ) + ".html\">PREV</a>|<a href=\"index.html\">LAST</a><p>" );
+		
+		writeLines( lines, "index.html" );
+	}
+	
+	public static ArrayList<String> readLines( String fileName )
+	{
+		File file = new File( fileName );
+		
+		if ( !file.exists() )
+		{
+			System.out.println( fileName + " does not exist" );
+			System.exit(1);
+		}
+		
+		BufferedReader reader = null;
+		ArrayList<String> lines = new ArrayList<String>();
+		
+		try
+		{
+			reader = new BufferedReader( new FileReader( file ) );
+			String line;
+			
+			while( ( line = reader.readLine() ) != null )
+			{
+				lines.add( line );
+			}
+			
+			reader.close();
+		}
+		catch( IndexOutOfBoundsException e )
+		{
+			System.out.println( fileName + " is incorrectly formatted" );
+			System.exit(4);
+		}
+		catch( IOException e )
+		{
+			System.out.println( "BufferedReader not created for " + fileName );
+			System.exit(5);
+		}
+		finally
+		{
+			return lines;
+		}
+	}
+	
+	public static void writeLines( ArrayList<String> lines, String fileName )
+	{
+		PrintWriter out = null;
+		
+		try
+		{
+			out = new PrintWriter( new BufferedWriter( new FileWriter( fileName ) ) );
+			
+			for( int j = 0; j < lines.size(); j++ )
+			{
+				out.println( lines.get( j ) );
+			}
+		}
+		catch ( IOException e )
+		{
+			System.out.println( "Writer not created for " + fileName );
+		}
+		finally
+		{
+			out.close();
+		}
+	}
+}
