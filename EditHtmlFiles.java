b84aee14330ce8f19b6dfd50c9954f2d13afb83a
Ronak Ramachandran <ironak314@gmail.com>
2016-05-16 21:02:03 -0400

    Mobile support?

--- EditHtmlFiles.java
+++ EditHtmlFiles.java
@@ -30,44 +30,44 @@ public class EditHtmlFiles
 		{
 			fileName = "noodles/" + String.format( "%04d", j ) + ".html";
 			
-			lines.set( 14, "		<p id=\"title\"><a name=\"comicView\">" + titles.get( j - 1 ) + "</a></p>" );
-			lines.set( 15, "		<img id=\"comic\" src=\"../comics/comic" + String.format( "%04d", j ) + ".JPG\" width=\"400\" height=\"400\" alt=\"" + mouseOver.get( j - 1 ) + "\" title = \"" + mouseOver.get( j - 1 ) + "\">" );
+			lines.set( 15, "		<p id=\"title\"><a name=\"comicView\">" + titles.get( j - 1 ) + "</a></p>" );
+			lines.set( 16, "		<img id=\"comic\" src=\"../comics/comic" + String.format( "%04d", j ) + ".JPG\" width=\"400\" height=\"400\" alt=\"" + mouseOver.get( j - 1 ) + "\" title = \"" + mouseOver.get( j - 1 ) + "\">" );
 			
 			if( j == 1 )
 			{
-				lines.set( 17, " 			<li><a class=\"inactive\" href=\"javascript:;\">FIRST</a></li><!--" );
-				lines.set( 18, " 			--><li><a class=\"inactive\" href=\"javascript:;\">PREV</a></li><!--" );
-				lines.set( 19, " 			--><li><a href=\"0002.html#comicView\">NEXT</a></li><!--" );
+				lines.set( 18, " 			<li><a class=\"inactive\" href=\"javascript:;\">FIRST</a></li><!--" );
+				lines.set( 19, " 			--><li><a class=\"inactive\" href=\"javascript:;\">PREV</a></li><!--" );
+				lines.set( 20, " 			--><li><a href=\"0002.html#comicView\">NEXT</a></li><!--" );
 			}
 			else if( j == maxNumber )
 			{
-				lines.set( 18, " 			--><li><a href=\"" + String.format( "%04d", j - 1 ) + ".html#comicView\">PREV</a></li><!--" );
-				lines.set( 19, " 			--><li><a class=\"inactive\" href=\"javascript:;\">NEXT</a></li><!--" );
-				lines.set( 20, " 			--><li><a class=\"inactive\" href=\"javascript:;\">LAST</a></li>" );
+				lines.set( 19, " 			--><li><a href=\"" + String.format( "%04d", j - 1 ) + ".html#comicView\">PREV</a></li><!--" );
+				lines.set( 20, " 			--><li><a class=\"inactive\" href=\"javascript:;\">NEXT</a></li><!--" );
+				lines.set( 21, " 			--><li><a class=\"inactive\" href=\"javascript:;\">LAST</a></li>" );
 			}
 			else
 			{
-				lines.set( 17, " 			<li><a href=\"0001.html#comicView\">FIRST</a></li><!--" );
-				lines.set( 18, " 			--><li><a href=\"" + String.format( "%04d", j - 1 ) + ".html#comicView\">PREV</a></li><!--" );
-				lines.set( 19, " 			--><li><a href=\"" + String.format( "%04d", j + 1 ) + ".html#comicView\">NEXT</a></li><!--" );
-				lines.set( 20, " 			--><li><a href=\"../index.html#comicView\">LAST</a></li>" );
+				lines.set( 18, " 			<li><a href=\"0001.html#comicView\">FIRST</a></li><!--" );
+				lines.set( 19, " 			--><li><a href=\"" + String.format( "%04d", j - 1 ) + ".html#comicView\">PREV</a></li><!--" );
+				lines.set( 20, " 			--><li><a href=\"" + String.format( "%04d", j + 1 ) + ".html#comicView\">NEXT</a></li><!--" );
+				lines.set( 21, " 			--><li><a href=\"../index.html#comicView\">LAST</a></li>" );
 			}
 			
-			lines.set( 22, "		<p id=\"description\">" + descriptions.get( j - 1 ) + "<p>" );
+			lines.set( 23, "		<p id=\"description\">" + descriptions.get( j - 1 ) + "<p>" );
 			
 			writeLines( lines, fileName );
 		}
 		
 		//index.html edits
 		lines.set( 4, "		<link rel=\"stylesheet\" href=\"style.css\">" );
-		lines.set( 10, "			<li><a href=\"javascript:;\">Home</a></li><!--" );
-		lines.set( 14, "		<p id=\"title\"><a name=\"comicView\">" + titles.get( maxNumber - 1 ) + "</a></p>" );
-		lines.set( 15, "		<img id=\"comic\" src=\"comics/comic" + String.format( "%04d", maxNumber ) + ".JPG\" width=\"400\" height=\"400\" alt=\"" + mouseOver.get( maxNumber - 1 ) + "\" title = \"" + mouseOver.get( maxNumber - 1 ) + "\">" );
-		lines.set( 17, " 			<li><a href=\"noodles/0001.html#comicView\">FIRST</a></li><!--" );
-		lines.set( 18, " 			--><li><a href=\"noodles/" + String.format( "%04d", maxNumber - 1 ) + ".html#comicView\">PREV</a></li><!--" );
-		lines.set( 19, " 			--><li><a class=\"inactive\" href=\"javascript:;\">NEXT</a></li><!--" );
-		lines.set( 20, " 			--><li><a class=\"inactive\" href=\"javascript:;\">LAST</a></li>" );
-		lines.set( 22, "		<p id=\"description\">" + descriptions.get( maxNumber - 1 ) + "<p>" );
+		lines.set( 11, "			<li><a href=\"javascript:;\">Home</a></li><!--" );
+		lines.set( 15, "		<p id=\"title\"><a name=\"comicView\">" + titles.get( maxNumber - 1 ) + "</a></p>" );
+		lines.set( 16, "		<img id=\"comic\" src=\"comics/comic" + String.format( "%04d", maxNumber ) + ".JPG\" width=\"400\" height=\"400\" alt=\"" + mouseOver.get( maxNumber - 1 ) + "\" title = \"" + mouseOver.get( maxNumber - 1 ) + "\">" );
+		lines.set( 18, " 			<li><a href=\"noodles/0001.html#comicView\">FIRST</a></li><!--" );
+		lines.set( 19, " 			--><li><a href=\"noodles/" + String.format( "%04d", maxNumber - 1 ) + ".html#comicView\">PREV</a></li><!--" );
+		lines.set( 20, " 			--><li><a class=\"inactive\" href=\"javascript:;\">NEXT</a></li><!--" );
+		lines.set( 21, " 			--><li><a class=\"inactive\" href=\"javascript:;\">LAST</a></li>" );
+		lines.set( 23, "		<p id=\"description\">" + descriptions.get( maxNumber - 1 ) + "<p>" );
 			
 		writeLines( lines, "index.html" );
 		
